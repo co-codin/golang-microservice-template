ae3a6b4e387744f05daa5625205035f152d50dae
elijah <cuiyeqing960904@gmail.com>
2023-06-26 09:09:32 -0400

    auth payload in broker service

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -4,6 +4,16 @@ import (
 	"net/http"
 )
 
+type RequestPayload struct {
+	Action string `json:"action"`
+	Auth AuthPayoload `json:"auth,omitempty"`
+}
+
+type AuthPayoload struct {
+
+}
+
+
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 	payload := jsonResponse{
 		Error: false,
@@ -11,4 +21,8 @@ func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 	}
 
 	_ = app.writeJSON(w, http.StatusOK, payload)
+}
+
+func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
+
 }
