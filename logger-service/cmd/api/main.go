66f176ba642d220e4406e293a68a9415bb3f2594
elijah <cuiyeqing960904@gmail.com>
2023-07-02 18:18:25 -0400

    connect to mongo func

--- logger-service/cmd/api/main.go
+++ logger-service/cmd/api/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
 const (
 	webPort = "80"
 	rpcPort = "5001"
@@ -7,6 +15,34 @@ const (
 	gRpcPort = "50001"
 )
 
+var client *mongo.Client
+
+type Config struct {
+
+}
+
 func main() {
-	
+	mongoClient, err := connectToMongo()
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	client = mongoClient
+}
+
+func connectToMongo() (*mongo.Client, error) {
+	clientOptions := options.Client().ApplyURI(mongoURL)
+	clientOptions.SetAuth(options.Credential{
+		Username: "admin",
+		Password: "password",
+	})
+
+	c, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Println("Error connection: ", err)
+		return nil, err
+	}
+
+	return c, nil
 }
