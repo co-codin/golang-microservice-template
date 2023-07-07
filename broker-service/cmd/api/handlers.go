c29db625f0ff484f0723c0b2a0c077cd52d44be0
elijah <cuiyeqing960904@gmail.com>
2023-07-07 08:48:42 -0400

    bug fixing

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -5,16 +5,24 @@ import (
 	"encoding/json"
 	"errors"
 	"net/http"
+
+	"golang.org/x/tools/go/analysis/passes/defers"
 )
 
 type RequestPayload struct {
 	Action string `json:"action"`
 	Auth AuthPayoload `json:"auth,omitempty"`
+	Log LogPayload `json:"log,omitempty"`
 }
 
 type AuthPayoload struct {
 	Email string `json:"email"`
 	Password string `json:"password"`
+}
+
+type LogPayload struct {
+	Name string `json:"name"`
+	Data string `json:"data"`
 }
 
 
@@ -40,9 +48,49 @@ func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
 	switch requestPayload.Action {
 		case "auth":
 			app.authenticate(w, requestPayload.Auth)
+
+		case "log":
+			app.logItem(w, requestPayload.Log)
 		default:
 			app.errorJSON(w, errors.New("unknow action"))
 	}
+}
+
+func (app *Config) logItem(w http.ResponseWriter, entry LogPayload) {
+	jsonData, _ := json.MarshalIndent(entry, "", "\t")
+
+
+	logServiceURL := "http://logger-service/log"
+
+	request, err := http.NewRequest("POST", logServiceURL, bytes.NewBuffer(jsonData))
+
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	request.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+
+	response, err := client.Do(request)
+
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusAccepted {
+		app.errorJSON(w, err)
+		return
+	}
+
+	var payload jsonResponse
+	payload.Error = false
+	payload.Message = "logged"
+
+	app.writeJSON(w, http.StatusAccepted, payload)
 }
 
 func (app *Config) authenticate(w http.ResponseWriter, a AuthPayoload) {
