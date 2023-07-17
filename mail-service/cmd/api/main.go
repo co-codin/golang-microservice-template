94fce00e91a1e29d45e07f4469e67864a26ff1ab
elijah <cuiyeqing960904@gmail.com>
2023-07-17 05:39:35 -0400

    creating mail struct

--- mail-service/cmd/api/main.go
+++ mail-service/cmd/api/main.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 )
 
 type Config struct {
-	
+	Mailer Mail
 }
 
 const webPort = "80"
@@ -26,4 +28,21 @@ func main() {
 	if err != nil {
 		log.Panic(err)
 	}
+}
+
+
+func createMail() Mail {
+	port, _ := strconv.Atoi(os.Getenv("MAIL_PORT"))
+	m := Mail{
+		Domain:      os.Getenv("MAIL_DOMAIN"),
+		Host:        os.Getenv("MAIL_HOST"),
+		Port:        port,
+		Username:    os.Getenv("MAIL_USERNAME"),
+		Password:    os.Getenv("MAIL_PASSWORD"),
+		Encryption:  os.Getenv("MAIL_ENCRYPTION"),
+		FromName:    os.Getenv("FROM_NAME"),
+		FromAddress: os.Getenv("FROM_ADDRESS"),
+	}
+
+	return m
 }
