73225c812b03f4d20400ef263a4321a6224b465f
elijah <cuiyeqing960904@gmail.com>
2023-07-19 17:03:16 -0400

    building event + consumer

---
+++ listener-service/event/consumer.go
@@ -0,0 +1,18 @@
+package event
+
+import (
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+type Consumer struct {
+	conn *amqp.Connection
+	queueName string
+}
+
+func NewConsumer(conn *amqp.Connection) (Consumer, error) {
+	consumer := Consumer{
+		conn: conn,
+	}
+
+	
+}
