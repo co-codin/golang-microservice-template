2c65b7c03df5c7259d81975f29df5c8dc8e46929
elijah <cuiyeqing960904@gmail.com>
2023-07-22 12:38:09 -0400

    listen for topics

--- listener-service/event/consumer.go
+++ listener-service/event/consumer.go
@@ -1,6 +1,9 @@
 package event
 
 import (
+	"encoding/json"
+	"fmt"
+
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -40,5 +43,56 @@ type Payload struct {
 }
 
 func (consumer *Consumer) Listen(topics []string) error {
-	
+	ch, err := consumer.conn.Channel()
+
+	if err != nil {
+		return err
+	}
+
+	defer ch.Close()
+
+	q, err := declareRandomQueue(ch)
+
+	if err != nil {
+		return err
+	}
+
+	for _, s := range topics {
+		ch.QueueBind(
+			q.Name,
+			s,
+			"logs_topic",
+			false,
+			nil,
+		)
+
+		if err != nil {
+			return err
+		}
+	}
+
+	messages, err := ch.Consume(q.Name, "", true, false, false, false, nil)
+
+	if err != nil {
+		return err
+	}
+
+	forever := make(chan bool)
+	go func() {
+		for d := range messages {
+			var payload Payload
+			_ = json.Unmarshal(d.Body, &payload)
+
+			go handlePayload(payload)
+		}
+	}()
+
+	fmt.Printf("Waiting for message [Exchange, Queue] [logs_topic, %s]\n", q.Name)
+	<- forever
+
+	return nil
+}
+
+func handlePayload(payload Payload) {
+
 }
